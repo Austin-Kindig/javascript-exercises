a35dac5fc94a1c5a3c0f7b49c46a95485313bede
Austin-Kindig <kindig.austin.ak@gmail.com>
2022-12-12 12:08:44 -0500

    complete challenge to create basic console-based calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(numberOne, numberTwo) {
+  return numberOne + numberTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(numberOne, numberTwo) {
+  return numberOne - numberTwo;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+  for(let i = 0; i < array.length; i++) {
+      total = total + array[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+  for(let i = 0; i < array.length; i++) {
+      total = total * array[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(numberOne, numberTwo) {
+  return numberOne ** numberTwo;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let factorial = 1;
+  for (let i = number; i > 1; i--) {
+      factorial = factorial * i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line

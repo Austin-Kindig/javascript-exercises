1f57da173257bc0c76988194013d6bbf1bf350dc
Austin-Kindig <kindig.austin.ak@gmail.com>
2022-12-12 12:06:04 -0500

    complete challenge to generate and return fibonacci sequence values

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 0) {
+        return "OOPS";
+    }
+    const fibonacciSequence = [1,1];
+    for (let i = 1; i < index; i++) {
+        fibonacciSequence.push(fibonacciSequence[i] + fibonacciSequence[i - 1]);
+    }
+    return fibonacciSequence[index - 1];
 };
 
 // Do not edit below this line

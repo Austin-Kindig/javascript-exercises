6797b8f89d72c723449afa2e2d77a92cd00f7ca6
Austin-Kindig <kindig.austin.ak@gmail.com>
2022-12-12 15:36:11 -0500

    complete challenge to sort array of objects (persons) by age

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const oldest = array.sort(function(first, second) {
+        if (first.yearOfDeath == undefined) {
+            first.yearOfDeath = new Date().getFullYear();
+        }
+        if (second.yearOfDeath == undefined) {
+            second.yearOfDeath = new Date().getFullYear();
+        }
+        const firstPerson = first.yearOfDeath - first.yearOfBirth;
+        const secondPerson = second.yearOfDeath - second.yearOfBirth;
+        return firstPerson > secondPerson ? -1 : 1;
+    })
+    return oldest[0];
 };
 
 // Do not edit below this line

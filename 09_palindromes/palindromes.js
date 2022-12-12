b8ce14abf25a301e3e9bd8c0e399b95340e3dc5a
Austin-Kindig <kindig.austin.ak@gmail.com>
2022-12-12 12:07:08 -0500

    complete challenge to test whether input string is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const originalString = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    const reversedString = [];
 
+    for(let i = originalString.length - 1; i >= 0; i--) {
+        reversedString.push(originalString[i]);
+    }
+    
+    if(reversedString.join('') === originalString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
